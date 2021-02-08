4cd9f2c9db560ebcb85ee532139c5bfb2e4884e0
shrishrimalshah-raj <shrishrimalraj@gmail.com>
2021-02-09 01:48:17 +0530

    feat: added package version

--- index1.js
+++ index1.js
@@ -11,24 +11,24 @@ const iPhone11 = devices["iPhone 11 Pro"];
     permissions: ["geolocation"],
   });
   const page = await context.newPage();
-  // page.on("response", (response) =>
-  //   console.log("<<", response.status(), response.headers())
-  // );
+  page.on("response", (response) =>
+    console.log("<<", response.status(), response.headers())
+  );
   await page.goto(
     "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
   );
 
-  const nifty = await page.innerText("body");
-  const niftyJSON = JSON.parse(nifty);
-  console.log("type of niftyJSON", typeof niftyJSON);
+  // const nifty = await page.innerText("body");
+  // const niftyJSON = JSON.parse(nifty);
+  // console.log("type of niftyJSON", typeof niftyJSON);
 
-  await page.goto(
-    "https://www.nseindia.com/api/option-chain-indices?symbol=BANKNIFTY"
-  );
+  // await page.goto(
+  //   "https://www.nseindia.com/api/option-chain-indices?symbol=BANKNIFTY"
+  // );
 
-  const niftyBank = await page.innerText("body");
-  const niftyBankJSON = JSON.parse(niftyBank);
-  console.log("type of niftyBankJSON", typeof niftyBankJSON);
+  // const niftyBank = await page.innerText("body");
+  // const niftyBankJSON = JSON.parse(niftyBank);
+  // console.log("type of niftyBankJSON", typeof niftyBankJSON);
 
   await browser.close();
 })();
